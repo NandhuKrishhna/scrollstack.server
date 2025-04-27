238158d54060bb1ab3c35ae6312b959ef04f5d35
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-28 04:30:25 +0530

    feat: add ArticlesController and AuthController with handlers for article and authentication operations

---
+++ src/controllers/article-controller.ts
@@ -0,0 +1,88 @@
+import { Request, Response } from "express";
+import { ArticlesControllerUseCase } from "../use-case/articleUseCase";
+import catchErrors from "../utils/catchErrors";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
+import { OK } from "../constants/http";
+import { stringToObjectId } from "../utils/bcrypt";
+
+
+export class ArticlesController {
+
+    constructor(private readonly __articleUseCase: ArticlesControllerUseCase) { }
+
+
+    addPreferencesHandler = catchErrors(async (req: Request, res: Response) => {
+        const { preferences } = req.body;
+        const { userId } = req as AuthenticatedRequest;
+        await this.__articleUseCase.addPreferences(preferences, userId);
+        return res.status(OK).json({
+            success: true,
+            message: "Preferences added successfully",
+        })
+    })
+
+    getAllArticlesHandler = catchErrors(async (req: Request, res: Response) => {
+        const articles = await this.__articleUseCase.getAllArticles();
+        return res.status(OK).json({
+            success: true,
+            message: "Articles fetched successfully",
+            data: articles
+        })
+    });
+
+    createArticleHandler = catchErrors(async (req: Request, res: Response) => {
+        const { userId: id } = req as AuthenticatedRequest;
+        const article = req.body;
+        const newArticle = await this.__articleUseCase.createArticle(id, article);
+        return res.status(OK).json({
+            success: true,
+            message: "Article created successfully",
+            data: newArticle
+        })
+    });
+
+    getArticlesOfUserHandler = catchErrors(async (req: Request, res: Response) => {
+        const id = stringToObjectId(req.params.id);
+        const articles = await this.__articleUseCase.getArticles(id);
+        return res.status(OK).json({
+            success: true,
+            message: "Articles fetched successfully",
+            data: articles
+        })
+    });
+
+    editArticleHandler = catchErrors(async (req: Request, res: Response) => {
+        const { userId } = req as AuthenticatedRequest;
+        const articleId = stringToObjectId(req.body.id);
+        const articleData = req.body.articleData;
+        const updatedArticle = await this.__articleUseCase.updateArticle(userId, articleId, articleData);
+        return res.status(OK).json({
+            success: true,
+            message: "Article updated successfully",
+            data: updatedArticle
+        })
+    });
+
+    deleteArticleHandler = catchErrors(async (req: Request, res: Response) => {
+        const { userId } = req as AuthenticatedRequest;
+        const articleId = stringToObjectId(req.body.id);
+        await this.__articleUseCase.deleteArticle(userId, articleId);
+        return res.status(OK).json({
+            success: true,
+            message: "Article deleted successfully",
+        })
+    });
+
+
+    likeArticleHanlder = catchErrors(async (req: Request, res: Response) => {
+        const { userId } = req as AuthenticatedRequest;
+        const articleId = stringToObjectId(req.body.articleId);
+        await this.__articleUseCase.likeArticle(userId, articleId)
+        return res.status(OK).json({
+            success: true,
+            message: "Successfully liked the post"
+        })
+    });
+
+
+}
---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express";
+import { AuthUseCase } from "../use-case/authUseCase";
+import catchErrors from "../utils/catchErrors";
+import { loginSchema, otpVerificationSchema, userRegisterSchema } from "../zod/user-registration";
+import { clearAuthCookies, generateRefreshTokenCookieOptions, getAccessTokenCookieOptions, setAuthCookies } from "../utils/setAuthCookies";
+import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
+import { stringToObjectId } from "../utils/bcrypt";
+import { verifyToken } from "../utils/jwt";
+import appAssert from "../utils/appAssert";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
+
+export class AuthController {
+    constructor(private readonly __authUseCase: AuthUseCase) { }
+
+
+
+    registrationHandler = catchErrors(async (req: Request, res: Response) => {
+        const userData = userRegisterSchema.parse({ ...req.body });
+        const { user, accessToken, refreshToken } = await this.__authUseCase.registerUser(userData);
+        return setAuthCookies({ res, accessToken, refreshToken })
+            .status(CREATED)
+            .json({
+                success: true,
+                message: `Registration successfull. An OTP has been sent to ${user.email}`,
+                response: { ...user, accessToken },
+            });
+
+    })
+    otpVerificationHandler = catchErrors(async (req: Request, res: Response) => {
+        const userId = stringToObjectId(req.body.userId);
+        const { code } = otpVerificationSchema.parse(req.body);
+        await this.__authUseCase.verifyOtp(code, userId);
+        return res.status(OK).json({
+            success: true,
+            message: "Email was successfully verfied",
+        });
+    })
+    loginHandler = catchErrors(async (req: Request, res: Response) => {
+        const userData = loginSchema.parse({
+            ...req.body,
+        });
+        const { accessToken, refreshToken, user } = await this.__authUseCase.loginUser(userData);
+        return setAuthCookies({ res, accessToken, refreshToken })
+            .status(OK)
+            .json({
+                message: "Login successful",
+                response: { ...user, accessToken },
+            });
+    });
+    refreshHandler = catchErrors(async (req: Request, res: Response) => {
+        const refreshToken = req.cookies.refreshToken as string | undefined;
+        appAssert(refreshToken, UNAUTHORIZED, "Missing refresh token, please log in again");
+        const { accessToken, newRefreshToken } = await this.__authUseCase.setRefreshToken(refreshToken);
+        if (newRefreshToken) {
+            res.cookie("refreshToken", newRefreshToken, generateRefreshTokenCookieOptions());
+        }
+        return res.status(OK).cookie("accessToken", accessToken, getAccessTokenCookieOptions()).json({
+            message: "Access token refreshed",
+            accessToken,
+        });
+    });
+
+    logoutHandler = catchErrors(async (req: Request, res: Response) => {
+        const accessToken = req.cookies.accessToken as string | undefined;
+        const { payload } = verifyToken(accessToken || "");
+        if (payload) {
+            await this.__authUseCase.logoutUser(payload);
+        }
+        return clearAuthCookies(res).status(OK).json({
+            message: "Logout successful",
+        });
+    });
+
+    updatePasswordHandler = catchErrors(async (req: Request, res: Response) => {
+        const { userId } = req as AuthenticatedRequest;
+        const { newPassword, oldPassword } = req.body
+        await this.__authUseCase.updatePassword(newPassword, oldPassword, userId)
+        return res.status(OK).json({
+            success: true,
+            message: "Password Updated Successfully"
+        })
+    })
+
+}
+
