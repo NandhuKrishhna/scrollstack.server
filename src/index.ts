84aa208772974721d7fe5e755e8a553975a32c45
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-29 23:30:08 +0530

    fix: update CORS origin to specific client URL for improved security

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,13 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: APP_ORIGIN,
+        origin: "https://scrollstack-client.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
+
 
 app.options("*", cors());
 app.use("/auth", authRouter);
